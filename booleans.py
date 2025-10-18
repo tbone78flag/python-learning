00c650b5afc4394b5e1f1462918c2f120b860caf
Tristan P. <tristan.tbone.pacheco@gmail.com>
2025-10-17 21:56:08 -0600

    Update booleans.py
    Learning Booleans and IF statements

--- booleans.py
+++ booleans.py
@@ -2,3 +2,26 @@
 Learning Booleans and IF statements
 (Compressed for learning)
 """
+
+day = 17 #Int (Integer)
+weight = 190.4 #Float
+month = 'October' #String
+
+light_is_on = True #Boolean
+
+#Going rouge with only IF statements
+if light_is_on:
+    print("The light is on!")
+    light_is_on = False
+else:
+    print("It's pretty dark in here.")
+    light_is_on = True
+
+if light_is_on:
+    print("*Click*")
+    print("Thank you for turning the light on.")
+    light_is_on = False
+else:
+    print("*Click*")
+    print("Why'd you turn the light off!?")
+    light_is_on = True
