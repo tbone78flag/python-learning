a2368e0b0393f1d0e7ed6dc01541de41b3d1b913
Tristan P. <tristan.tbone.pacheco@gmail.com>
2025-10-17 21:22:43 -0600

    Variables and Strings
    Did three basic lessons on variables
    Added learning section for Strings

---
+++ variables.py
@@ -0,0 +1,34 @@
+"""
+Learning to Create Variables
+(Compressed for space while learning)
+"""
+wallet = 41
+print(wallet)
+wallet = 32
+print(wallet)
+day = 17
+month = "October"
+print(month, day)
+print("$", wallet)
+
+"""
+Learning about Ints and Floats
+"""
+
+day = 17
+temp = -15
+weight = 190.4
+
+print("Example of Int (Integer):", day)
+print("Example of Negative Int (Integer):", temp)
+print("Example of Float:", weight)
+
+"""
+Doing Math
+"""
+
+print(3 + 6)
+print(day + 6)
+print(weight * 2)
+print(weight / 2)
+print(temp - 2)
