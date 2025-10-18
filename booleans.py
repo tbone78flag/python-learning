77893c346b871741a02283201f6b31732b0e684f
Tristan P. <tristan.tbone.pacheco@gmail.com>
2025-10-17 22:51:14 -0600

    Update booleans.py
    Added challenge and learned modulos

--- booleans.py
+++ booleans.py
@@ -42,3 +42,18 @@ if weight != 185: #!= is not equal to
 if weight == 190: #== is equal to
     print(f"You are currently at {weight} lbs which is only {weight_sub} lbs from your goal weight of {goal_weight} lbs.")
 #<= is equal to or less than and >= is equal to or greater than
+
+"""
+Challenge: Answering if a number is odd
+"""
+def your_code():
+    number = 7
+
+    result = number % 2 #Using a modulo to check if there is a remainder of 1
+
+    if result == 0:
+        return
+    else: 
+        return True
+        
+    return False
